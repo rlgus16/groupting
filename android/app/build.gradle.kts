0b02ee29bdf218bbff728e152f8d8b9cafaedee8
rlgus16 <rlgus165@gmail.com>
2025-12-03 15:17:53 +0900

    앱 사인
    +플레이스토어 리스팅에 필요한 앱 사인

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,11 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
@@ -41,4 +49,24 @@ flutter {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+signingConfigs {
+    create("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
+        storeFile = file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["storePassword"] as String
+    }
+}
+
+buildTypes {
+    getByName("release") {
+        // Change this from "debug" to "release"
+        signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = true // Recommended for code shrinking
+        isShrinkResources = true // Recommended
+        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+}
 }
